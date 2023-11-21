956315836e216c71064978ba13ef69aa8a7147a2
TaleOfTwoCities <jiabrilmthzi@gmail.com>
2023-11-21 16:45:05 +0500

    2nd commit

--- sev.js
+++ sev.js
@@ -5,7 +5,7 @@ const app =  express();
 app.get("/",(req,res) => {
     res.sendFile(path.join(__dirname + "/ind.html"));
 })
-
+const M=""
 const server = app.listen(8545);
 const portnumber = server.address().port
 console.log("port is open on "+ portnumber);
